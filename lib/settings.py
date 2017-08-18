9be39aa31252cf53d3ae1280623e566b588060a4
Kevin Navero <knavero@gmail.com>
2017-08-17 20:08:45 -0700

    finally got recursive list checking working

--- lib/settings.py
+++ lib/settings.py
@@ -1,27 +1,130 @@
-import json
 from lib.custom_exceptions import *
 from collections.abc import Mapping
 
+import json
+import re
+
+
 class Settings(Mapping):
    """class for accessing settings"""
+
+
+   @staticmethod
+   def _is_primitive(val):
+      prims = [int, float, str, bool]
+      for prim in prims:
+         if isinstance(val, prim):
+            return True
+      return False
+
+   @staticmethod
+   def _is_list(val):
+      return isinstance(val, list)
+
+   @staticmethod
+   def _is_dict(val):
+      return isinstance(val, dict)
+
+   @staticmethod
+   def _wildcard_validity_check(key, val):
+      # TODO
+      # glob_pat = re.compile(r'\*(:(?P<type>\w+))?')
+      # for s in val[setting]:
+      #    m = glob_pat.match(s)
+      #    if m:
+      #       m.group('type')
+      pass
+
+   @staticmethod
+   def _is_in_prim(v, valid_v):
+      return v in valid_v
+
+   @staticmethod
+   def get_type_to_is_in():
+      return {
+         'primitive': Settings._is_in_prim,
+         'list': Settings._is_in_list,
+         'dict': Settings._is_in_dict
+      }
+
+   @staticmethod
+   def _is_sublist_in_one_of_lists(sublist, lists):
+      type_to_is_in = Settings.get_type_to_is_in()
+
+      for vl in lists:
+         next_vl = False
+         for e in sublist:
+            if Settings._is_primitive(e):
+               t = 'primitive'
+            elif Settings._is_list(e):
+               e = [e]
+               t = 'list'
+            elif Settings._is_dict(e):
+               t = 'dict'
+            else:
+               raise InvalidSettingError()
+
+            if not type_to_is_in[t](e, vl):
+               next_vl = True
+               break
+
+         if next_vl:
+            continue
+         return True
+      return False
+
+   @staticmethod
+   def _is_in_list(l, valid_l):
+      for elem in l:
+         if Settings._is_primitive(elem):
+            if not Settings._is_in_prim(elem, valid_l):
+               return False
+         elif Settings._is_list(elem):
+            valid_lists = [l for l in valid_l if isinstance(l, list)]
+            if not Settings._is_sublist_in_one_of_lists(elem, valid_lists):
+               return False
+         elif Settings._is_dict(elem):
+            if not Settings._is_in_dict(elem, valid_l):
+               return False
+      return True
+
+   @staticmethod
+   def _is_in_dict():
+      pass
+
+   @staticmethod
+   def _primitive_validity_check(v, valid_v):
+      if not Settings._is_in_prim(v, valid_v):
+         raise InvalidSettingError()
+
+   @staticmethod
+   def _list_validity_check(l, valid_l):
+      idx = 0
+      for elem in l:
+         if Settings._is_primitive(elem):
+            Settings._primitive_validity_check(elem, valid_l[idx])
+         idx += 1
+
+   @staticmethod
+   def _dict_validity_check(d, valid_d):
+      for k, v in d.items():
+         if Settings._is_primitive(v):
+            Settings._primitive_validity_check(v, valid_d[k])
+         elif isinstance(v, list):
+            Settings._list_validity_check(v, valid_d[k])
+         elif isinstance(v, dict):
+            Settings._dict_validity_check(v, valid_d[k])
 
    @staticmethod
    def _validity_check(settings, valid):
       """error check |settings| and |valid|. Both are dict types. |settings|
       represents the user settings where each pair is a setting name associated
       to a chosen setting value. |valid| represents all valid user settings
-      where each pair is a setting name associated to a list of possible valid
-      setting values. An empty list value will raise a NoOptsError, and a
-      setting value in |settings| that does not exist in the associated valid
-      values list will raise an InvalidSettingError.
+      where each pair is a setting name associated to legal valid
+      setting values.
       """
 
-      for setting, opts in valid.items():
-         if len(opts) == 0:
-            raise NoOptsError(setting)
-      for setting, option in settings.items():
-         if setting not in valid or option not in valid[setting]:
-            raise InvalidSettingError(setting, option)
+      Settings._dict_validity_check(settings, dict)
 
    @staticmethod
    def _init_defaults(settings, valid):
--- tests/settings_test.py
+++ tests/settings_test.py
@@ -7,111 +7,176 @@ from unittest.mock import MagicMock
 from tests.mock_restore import MockRestore
 
 class SettingsTest(unittest.TestCase):
-   def setUp(self):
-      self.settings = Settings({'foo': 1, 'bar': 'barval'},
-                               {'foo': [1], 'bar': ['barval']})
-      self.empty_settings = Settings({}, {})
-      self.same =  Settings({'bar':'barval','foo':1},
-               {'foo':[1],'bar':['barval']})
-      self.slightly_diff = Settings({'bar': 'barval', 'fooo': 1},
-                           {'fooo': [1], 'bar': ['barval']})
 
-      with open('foo_settings.json', 'w') as settings:
-         json.dump({'foo': 1, 'bar': 'barval'}, settings)
+   def test_is_in_prim(self):
+      self.assertTrue(Settings._is_in_prim('z', ['x', 'y', 'z']))
+      self.assertTrue(Settings._is_in_prim('x', ['x', 'y', 'z']))
+      self.assertTrue(Settings._is_in_prim('y', ['x', 'y', 'z']))
+      self.assertFalse(Settings._is_in_prim('a', ['x', 'y', 'z']))
 
-   def test_ctor(self):
-      s = Settings({'foo': 1}, {'foo': [0, 1]})
-      self.assertIsInstance(s, Settings)
-      s = Settings({'foo': 0}, {'foo': [0, 1]})
-      self.assertIsInstance(s, Settings)
-      s = Settings({'foo': 0, 'bar': 'barval'},
-                   {'foo': [0, 1], 'bar': ['barval', 'barval2']})
-      self.assertIsInstance(s, Settings)
-      s = Settings({'foo': 0, 'bar': 'barval2'},
-                   {'foo': [0, 1], 'bar': ['barval', 'barval2']})
-      self.assertIsInstance(s, Settings)
+   def test_is_in_list(self):
+      self.assertTrue(Settings._is_in_list([['x']], [['z'], ['x','y']]))
+      self.assertTrue(Settings._is_in_list([['x']], [['z'], ['x','a']]))
+      self.assertFalse(Settings._is_in_list([['x']], [['z'], ['b','a']]))
+      self.assertTrue(Settings._is_in_list([['x']], [['z'], ['b','a'], ['y','x']]))
+      self.assertTrue(Settings._is_in_list([['y']], [['z'], ['b','a'], ['y','x']]))
+      self.assertFalse(Settings._is_in_list([['c']], [['z'], ['b','a'], ['y','x']]))
+      self.assertFalse(Settings._is_in_list([['c','x']], [['z'], ['b','a'], ['y','x']]))
+      self.assertFalse(Settings._is_in_list([['c','x']], [['z'], ['b','c','a'], ['y','x']]))
+      self.assertTrue(Settings._is_in_list([['c','x']], [['z'], ['b','c','a'], ['y','x','z','c','f']]))
+      self.assertTrue(Settings._is_in_list([['c','x','z']], [['z'], ['b','c','a'], ['y','x','z','c','f']]))
+      self.assertFalse(Settings._is_in_list([['c','x','b']], [['z'], ['b','c','a'], ['y','x','z','c','f']]))
+      self.assertTrue(Settings._is_in_list([[[[['a'], ['y']]]]], [[[[['a', 'b', 'c', 'y', 'z']]]]]))
+      self.assertFalse(Settings._is_in_list([[[['a'], ['y']]]], [[[[['a', 'b', 'c', 'y', 'z']]]]]))
+      self.assertTrue(Settings._is_in_list([[[[['a','c'], ['y']]]]], [[[[['y', 'z'], ['a', 'b', 'c']]]]]))
+      self.assertFalse(Settings._is_in_list([[[['a','c'], ['y']]]], [[[[['y','z'],['a', 'b', 'c']]]]]))
+      self.assertFalse(Settings._is_in_list([[[[['a','y'], ['c']]]]], [[[[['y', 'z'], ['a', 'b', 'c']]]]]))
 
-      with self.assertRaises(InvalidSettingError):
-         s = Settings({'foo': 1}, {'foo': [0, 2]})
-      with self.assertRaises(InvalidSettingError):
-         s = Settings({'foo': 0, 'bar': 'barval'},
-                   {'foo': [0, 1], 'bar': ['barval1', 'barval2']})
-
-      with self.assertRaises(NoOptsError):
-         s = Settings({'foo':0, 'bar':'barr'}, {'foo':[1, 0], 'bar':[]})
-
-      s = Settings({}, {'foo':[1, 0], 'bar':['barval', 'barval2']})
-      self.assertEqual(s['bar'], 'barval')
-      self.assertEqual(s['foo'], 1)
-
-   def test_ctor_with_settings_file(self):
-      s = Settings('foo_settings.json',
-                   {'foo':[0, 1], 'bar':['barval', 'barval2']})
-      with self.assertRaises(InvalidSettingError):
-         Settings('foo_settings.json',
-                  {'foo':[0, 2], 'bar':['barval', 'barval2']})
-      with self.assertRaises(InvalidSettingError):
-         Settings('foo_settings.json',
-                  {'foo':[0, 1], 'bar':['barval2', 'barval3']})
-
-   def test_getitem(self):
-      s = self.settings
-      self.assertEqual(s['foo'], 1)
-      self.assertEqual(s['bar'], 'barval')
-      with self.assertRaises(KeyError):
-         s['baz']
-
-   def test_iter(self):
-      s = self.settings
-      self.assertEqual(sorted(list(iter(s))), ['bar', 'foo'])
-      s = self.empty_settings
-      self.assertEqual(list(iter(s)), [])
-
-   def test_len(self):
-      s = self.settings
-      self.assertEqual(len(s), 2)
-      s = self.empty_settings
-      self.assertEqual(len(s), 0)
-
-   def test_contains(self):
-      s = self.settings
-      self.assertTrue('foo' in s)
-      self.assertTrue('bar' in s)
-      self.assertFalse('baz' in s)
-      s = self.empty_settings
-      self.assertFalse('foo' in s)
-
-   def test_keys(self):
-      s = self.settings
-      self.assertEqual(sorted(s.keys()), ['bar', 'foo'])
-      s = self.empty_settings
-      self.assertEqual(list(s.keys()), [])
-
-   def test_values(self):
-      s = self.settings
-      self.assertEqual(sorted([str(v) for v in s.values()]), ['1', 'barval'])
-      self.assertTrue(1 in s.values())
-      self.assertTrue('barval' in s.values())
-
-   def test_get(self):
-      s = self.settings
-      self.assertEqual(s.get('foo'), 1)
-      self.assertEqual(s.get('bar'), 'barval')
-      self.assertIsNone(s.get('baz'))
-      self.assertEqual(s.get('baz', 0), 0)
-      s = self.empty_settings
-      self.assertIsNone(s.get('foo'))
-
-   def test_eq(self):
-      s = self.settings
-      s2 = self.same
-      self.assertEqual(s, s2)
-      self.assertIsNot(s, s2)
-
-   def test_ne(self):
-      s = self.settings
-      self.assertNotEqual(s, self.slightly_diff)
-      self.assertNotEqual(s, self.empty_settings)
-
-   def tearDown(self):
-      os.remove('foo_settings.json')
+   # def setUp(self):
+   #    self.settings = Settings({'foo': 1, 'bar': 'barval'},
+   #                             {'foo': [1], 'bar': ['barval']})
+   #    self.empty_settings = Settings({}, {})
+   #    self.same =  Settings({'bar':'barval','foo':1},
+   #             {'foo':[1],'bar':['barval']})
+   #    self.slightly_diff = Settings({'bar': 'barval', 'fooo': 1},
+   #                         {'fooo': [1], 'bar': ['barval']})
+   #
+   #    with open('foo_settings.json', 'w') as settings:
+   #       json.dump({'foo': 1, 'bar': 'barval'}, settings)
+   #
+   # def test_ctor(self):
+   #    s = Settings({'foo': 1}, {'foo': [0, 1]})
+   #    self.assertIsInstance(s, Settings)
+   #    s = Settings({'foo': 0}, {'foo': [0, 1]})
+   #    self.assertIsInstance(s, Settings)
+   #    s = Settings({'foo': 0, 'bar': 'barval'},
+   #                 {'foo': [0, 1], 'bar': ['barval', 'barval2']})
+   #    self.assertIsInstance(s, Settings)
+   #    s = Settings({'foo': 0, 'bar': 'barval2'},
+   #                 {'foo': [0, 1], 'bar': ['barval', 'barval2']})
+   #    self.assertIsInstance(s, Settings)
+   #
+   #    with self.assertRaises(InvalidSettingError):
+   #       s = Settings({'foo': 1}, {'foo': [0, 2]})
+   #    with self.assertRaises(InvalidSettingError):
+   #       s = Settings({'foo': 0, 'bar': 'barval'},
+   #                 {'foo': [0, 1], 'bar': ['barval1', 'barval2']})
+   #
+   #    with self.assertRaises(NoOptsError):
+   #       s = Settings({'foo':0, 'bar':'barr'}, {'foo':[1, 0], 'bar':[]})
+   #
+   #    # TODO: implement multiple values in list validation
+   #    s = Settings({'foo': ['a', 'b']}, {'foo': ['a', 'b', 'c']})
+   #
+   #    # TODO: implement dicts contained in list validation
+   #    s = Settings(
+   #       {'foo': [{'bar': 3}, {'baz': 4}]},
+   #       {'foo': [
+   #          {'bar': [4, 3]},
+   #          {'baz': [4, 5]},
+   #          {'mu': [6, 7]}
+   #       ]})
+   #
+   #    # TODO implment nested dict validation
+   #    s = Settings(
+   #       {'foo': {'bar': 'baz'}},
+   #       {'foo': [
+   #          {'bar': ['mu', 'baz']},
+   #          {'gar': ['gu', 'gaz']}
+   #       ]})
+   #
+   # def test_defaults(self):
+   #    s = Settings({}, {'foo': [1, 0], 'bar': ['barval', 'barval2']})
+   #    self.assertEqual(s['bar'], 'barval')
+   #    self.assertEqual(s['foo'], 1)
+   #
+   # def test_ctor_with_settings_file(self):
+   #    s = Settings('foo_settings.json',
+   #                 {'foo':[0, 1], 'bar':['barval', 'barval2']})
+   #    with self.assertRaises(InvalidSettingError):
+   #       Settings('foo_settings.json',
+   #                {'foo':[0, 2], 'bar':['barval', 'barval2']})
+   #    with self.assertRaises(InvalidSettingError):
+   #       Settings('foo_settings.json',
+   #                {'foo':[0, 1], 'bar':['barval2', 'barval3']})
+   #
+   #
+   #
+   # def test_wildcard(self):
+   #    # TODO: implement '*' wildcard with type specifier for instance, '*:str' meaning any string
+   #    s = Settings({'foo': 1}, {'foo': '*'})
+   #    s = Settings({'foo': 'val'}, {'foo': '*:str'})
+   #    s = Settings({'foo': 'val'}, {'foo': '*:int,str'})
+   #    s = Settings({'foo': 2.4}, {'foo': '*:float'})
+   #    s = Settings({'foo': True}, {'foo': '*:bool'})
+   #
+   #    with self.assertRaises(InvalidSettingError):
+   #       s = Settings({'foo': 'val'}, {'foo': '*:int'})
+   #    with self.assertRaises(InvalidSettingError):
+   #       s = Settings({'foo': 'val'}, {'foo': '*:bool'})
+   #    with self.assertRaises(InvalidSettingError):
+   #       s = Settings({'foo': 'val'}, {'foo': '*:float'})
+   #    with self.assertRaises(InvalidSettingError):
+   #       s = Settings({'foo': True}, {'foo': '*:str'})
+   #
+   # def test_getitem(self):
+   #    s = self.settings
+   #    self.assertEqual(s['foo'], 1)
+   #    self.assertEqual(s['bar'], 'barval')
+   #    with self.assertRaises(KeyError):
+   #       s['baz']
+   #
+   # def test_iter(self):
+   #    s = self.settings
+   #    self.assertEqual(sorted(list(iter(s))), ['bar', 'foo'])
+   #    s = self.empty_settings
+   #    self.assertEqual(list(iter(s)), [])
+   #
+   # def test_len(self):
+   #    s = self.settings
+   #    self.assertEqual(len(s), 2)
+   #    s = self.empty_settings
+   #    self.assertEqual(len(s), 0)
+   #
+   # def test_contains(self):
+   #    s = self.settings
+   #    self.assertTrue('foo' in s)
+   #    self.assertTrue('bar' in s)
+   #    self.assertFalse('baz' in s)
+   #    s = self.empty_settings
+   #    self.assertFalse('foo' in s)
+   #
+   # def test_keys(self):
+   #    s = self.settings
+   #    self.assertEqual(sorted(s.keys()), ['bar', 'foo'])
+   #    s = self.empty_settings
+   #    self.assertEqual(list(s.keys()), [])
+   #
+   # def test_values(self):
+   #    s = self.settings
+   #    self.assertEqual(sorted([str(v) for v in s.values()]), ['1', 'barval'])
+   #    self.assertTrue(1 in s.values())
+   #    self.assertTrue('barval' in s.values())
+   #
+   # def test_get(self):
+   #    s = self.settings
+   #    self.assertEqual(s.get('foo'), 1)
+   #    self.assertEqual(s.get('bar'), 'barval')
+   #    self.assertIsNone(s.get('baz'))
+   #    self.assertEqual(s.get('baz', 0), 0)
+   #    s = self.empty_settings
+   #    self.assertIsNone(s.get('foo'))
+   #
+   # def test_eq(self):
+   #    s = self.settings
+   #    s2 = self.same
+   #    self.assertEqual(s, s2)
+   #    self.assertIsNot(s, s2)
+   #
+   # def test_ne(self):
+   #    s = self.settings
+   #    self.assertNotEqual(s, self.slightly_diff)
+   #    self.assertNotEqual(s, self.empty_settings)
+   #
+   # def tearDown(self):
+   #    os.remove('foo_settings.json')
